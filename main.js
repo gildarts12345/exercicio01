b27a1fd53863d77d6fad2ba21be8550d18cedb60
gildarts12345 <johnnywillian12345@hotmail.com>
2020-12-05 17:33:39 -0300

    linha para conflitar

---
+++ main.js
@@ -0,0 +1,14 @@
+var x = "Johnny tem ";
+var y = 20 ;
+var z = " Anos";
+
+alert(x + y+ z);
+
+
+var nome = prompt("Qual é o seu nome?")
+
+var idade = prompt("Qual é a sua idade?")
+
+console.log("Nome: ", nome)
+
+console.log("Idade: ", idade)
